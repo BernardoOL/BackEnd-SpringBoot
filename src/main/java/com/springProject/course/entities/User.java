40d44df3463a4110aa24f6bc336856a39548e08a
BernardoLopes <bernardomak42@gmail.com>
2025-05-07 12:23:58 -0300

    Create database and start mapping JPA

--- src/main/java/com/springProject/course/entities/User.java
+++ src/main/java/com/springProject/course/entities/User.java
@@ -1,6 +1,12 @@
 package com.springProject.course.entities;
 
 import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,10 +18,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 
     private static final long serialVersionUID = 1L;
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
