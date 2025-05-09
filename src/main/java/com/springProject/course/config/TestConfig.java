fe767675c3438e93218f0be8cb49ad3697fb5cd1
BernardoLopes <bernardomak42@gmail.com>
2025-05-08 21:22:47 -0300

    Creating enum OrderStatus

--- src/main/java/com/springProject/course/config/TestConfig.java
+++ src/main/java/com/springProject/course/config/TestConfig.java
@@ -3,7 +3,6 @@ package com.springProject.course.config;
 import java.time.Instant;
 import java.util.Arrays;
 
-import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.springProject.course.entities.Order;
 import com.springProject.course.entities.User;
+import com.springProject.course.entities.enums.OrderStatus;
 import com.springProject.course.repository.OrderRepository;
 import com.springProject.course.repository.UserRepository;
 
@@ -31,9 +31,9 @@ public class TestConfig implements CommandLineRunner{
 
         userRepository.saveAll(Arrays.asList(user1, user2));
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
 
