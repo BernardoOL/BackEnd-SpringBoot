5de633431686ffe4ad713f1504e3500287d39445
BernardoLopes <bernardomak42@gmail.com>
2025-05-08 21:55:18 -0300

    Create Category entity, resource, service and repository

--- src/main/java/com/springProject/course/config/TestConfig.java
+++ src/main/java/com/springProject/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.springProject.course.entities.Category;
 import com.springProject.course.entities.Order;
 import com.springProject.course.entities.User;
 import com.springProject.course.entities.enums.OrderStatus;
+import com.springProject.course.repository.CategoryRepository;
 import com.springProject.course.repository.OrderRepository;
 import com.springProject.course.repository.UserRepository;
 
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner{
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456", null);
@@ -36,6 +41,12 @@ public class TestConfig implements CommandLineRunner{
         Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
     }
 
