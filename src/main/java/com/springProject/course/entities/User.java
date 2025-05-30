086a9004ea1eb5b317074e899d94ea5aa22ef806
BernardoLopes <bernardomak42@gmail.com>
2025-05-29 21:40:15 -0300

    Update classe user for new Spring Project

--- src/main/java/com/springProject/course/entities/User.java
+++ src/main/java/com/springProject/course/entities/User.java
@@ -1,17 +1,12 @@
 package com.springProject.course.entities;
 
 import java.io.Serializable;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -21,7 +16,7 @@ import lombok.*;
 @Setter
 @Builder
 @Entity
-@Table(name = "tb_user")
+@Table(name = "TB_USER")
 public class User implements Serializable{
 
     private static final long serialVersionUID = 1L;
@@ -32,25 +27,13 @@ public class User implements Serializable{
 
     private String name;
 
-    private String birthDate;
-
     private String email;
 
     private String phone;
 
-    private String adress;
+    private String birthDate;
 
-    private String gender;
-
-    private Instant Inclusion;
-
-    // TODO - Tirar a senha
     private String password;
-
-    @Builder.Default
-    @JsonIgnore
-    @OneToMany(mappedBy = "client")
-    private List<Order> orders = new ArrayList<>();
 
     @Override
     public int hashCode() {
