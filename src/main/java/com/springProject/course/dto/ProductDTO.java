e1ae88acaf0d8d9eceb46462b76875e71ddbdcac
BernardoLopes <bernardomak42@gmail.com>
2025-06-19 22:12:54 -0300

    Remove unused contructor

--- src/main/java/com/springProject/course/dto/ProductDTO.java
+++ src/main/java/com/springProject/course/dto/ProductDTO.java
@@ -19,14 +19,6 @@ public class ProductDTO {
     public ProductDTO() {
     }
 
-    public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.imgUrl = imgUrl;
-    }
-
     public ProductDTO(Product product) {
         id = product.getId();
         name = product.getName();
